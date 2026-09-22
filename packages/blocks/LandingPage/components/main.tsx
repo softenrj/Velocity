b0a02907c4a90a38077811fab6394c54edbf848b
Softenrj <rjsharmase@gmail.com>
2026-09-23 01:26:37 +0530

    chore: update pnpm workspace configuration with new catalog dependencies and set catalogMode to prefer

---
+++ packages/blocks/LandingPage/components/main.tsx
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Raj
+// See LICENSE for details.
+
+import React from 'react'
+import { Badge } from "@velocity/ui";
+
+function main() {
+    return (
+        <div className='min-h-screen w-full flex justify-center items-center'>
+            <Badge variant="secondary">Secondary</Badge>
+        </div>
+    )
+}
+
+export default main
---
+++ packages/blocks/LandingPage/index.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2026 Raj
+// See LICENSE for details.
+
+import Main from "./components/main.tsx";
+
+export default function LandingPage() {
+    return <Main />
+}
